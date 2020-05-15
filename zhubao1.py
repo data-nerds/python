3a9eccac156f21407f79872f20be1bac62b8022a
zhubaomama <zhubaomama@zhubaomamas-MBP.lan>
2020-05-15 14:16:18 -0500

    my python training code as of May 15

---
+++ zhubao1.py
@@ -0,0 +1,29 @@
+secret = "i love you guys"
+my_list = [1, 2, 3]
+print(my_list)
+my_list.append(4)
+print(my_list)
+
+# ghtj
+
+print(my_list[1])
+
+list2 = [secret, "honey tree 2", "loving tree 3"]
+print(list2)
+
+var1 = "I love sabrina"
+var2 = "I love sophia"
+var3 = "I love mommy"
+var4 = "I love daddy"
+
+print(var1, var2, var3, var4)
+
+print(1 < 2 and 3 > 4)
+
+if 1 < 2:
+    print('Yahoo')
+
+i = 1
+while i <= 5:
+    print('i is: {}'.format(i))
+    i = i + 1
